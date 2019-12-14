3e1fe20131c89236bd644bb5393d715385e6791a
Michal Wadas <s20495@pjwstk.edu.pl>
2019-12-14 13:41:51 +0100

    class

---
+++ klasa.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+import sys
+class klasa:
+    def __init__(self,x):
+        self.x = x
+    def string(self, s):
+        return self.x.join(s)
+k=klasa('x')
+print(k.string(sys.argv))
